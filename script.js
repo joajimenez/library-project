b882092de3a5e50a015a0a2e8f1ef7d36839151d
Joan Jimenez <jimenez.donaldson@gmail.com>
2023-08-12 10:42:26 -0400

    Refactored removeFromLibrary() function. Now it can delete the correct book again.

--- script.js
+++ script.js
@@ -109,17 +109,38 @@ function changeReadStatus() {
     });
 }
 
-function removeBookFromLibrary() {
-    ui.libraryContainer.addEventListener('click', (event) => {
-        const target = event.target;
-        if (target.classList.contains('remove-book')) {
-            const bookDiv = target.closest('.card');
-            const bookIndex = parseInt(bookDiv.getAttribute('data-index'));
+// function removeBookFromLibrary() {
+//     ui.libraryContainer.addEventListener('click', (event) => {
+//         const target = event.target;
+//         if (target.classList.contains('remove-book')) {
+//             const bookDiv = target.closest('.card');
+//             const bookIndex = parseInt(bookDiv.getAttribute('data-index'));
+//
+//             myLibrary.splice(bookIndex, 1);
+//             displayBooks(myLibrary);
+//         }
+//     });
+// }
 
+function removeBookFromLibrary() {
+    const libraryContainer = ui.libraryContainer;
+    const bookDivs = libraryContainer.querySelectorAll('.card');
+    for (let i = 0; i < bookDivs.length; i++) {
+        const bookCard = bookDivs[i];
+        const removeButton = bookCard.querySelector('.remove-book');
+        removeButton.addEventListener('click', function () {
+            // Get the book index from the data-index attribute of the book card
+            const bookIndex = parseInt(bookCard.getAttribute('data-index'));
+
+            // Remove the book card from the DOM
+            bookCard.remove();
+
+            // Remove the book object from the myLibrary array
             myLibrary.splice(bookIndex, 1);
-            displayBooks(myLibrary);
-        }
-    });
+
+            console.log(`Book ${bookIndex} removed`);
+        });
+    }
 }
 
 function main() {
@@ -147,7 +168,10 @@ function main() {
         removeAllBooks();
     });
 
+    removeBookFromLibrary()
+
     displayBooks(myLibrary);
+
 }
 
 main();
