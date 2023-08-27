87ffbd403a2016f062016949b586ccb6a33d96ef
Joan Jimenez <jimenez.donaldson@gmail.com>
2023-08-27 23:04:13 +0000

    Fixed mobile view version

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const ui = {
-  libraryContainer: document.getElementById('library-container-el'),
-  addBookButton: document.querySelector('.add-book-btn'),
-  removeAllBooksButton: document.querySelector('.remove-all-btn'),
-  dialog: document.querySelector('#add-book-dialog'),
-  addBookDialog: document.querySelector('#add-book-dialog'),
-  addBookForm: document.querySelector('#add-book-form'),
-  formSubmitButton: document.querySelector('.form-submit-btn'),
-  formCancelButton: document.querySelector('.form-cancel-btn'),
-  readStatusInput: document.querySelector('#book-is-read'),
+  libraryContainer: document.getElementById("library-container-el"),
+  addBookButton: document.querySelector(".add-book-btn"),
+  removeAllBooksButton: document.querySelector(".remove-all-btn"),
+  dialog: document.querySelector("#add-book-dialog"),
+  addBookDialog: document.querySelector("#add-book-dialog"),
+  addBookForm: document.querySelector("#add-book-form"),
+  formSubmitButton: document.querySelector(".form-submit-btn"),
+  formCancelButton: document.querySelector(".form-cancel-btn"),
+  readStatusInput: document.querySelector("#book-is-read"),
 };
 
 class Book {
@@ -20,16 +20,56 @@ class Book {
 }
 
 const myLibrary = [
-  new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'Read'),
-  new Book('The Fellowship of the Ring', 'J.R.R. Tolkien', 423, 'Read'),
-  new Book('The NeverEnding Story', 'Michael Ende', 368, 'Read'),
+  new Book("The Hobbit", "J.R.R. Tolkien", 295, "Read"),
+  new Book("The Fellowship of the Ring", "J.R.R. Tolkien", 423, "Read"),
+  new Book("The NeverEnding Story", "Michael Ende", 368, "Read"),
   new Book(
-    'Little Bets: How Breakthrough Ideas Emerge from Small Discoveries',
-    'Peter Sims',
+    "Little Bets: How Breakthrough Ideas Emerge from Small Discoveries",
+    "Peter Sims",
     189,
-    'Not Read'
+    "Not Read"
   ),
 ];
+
+class Library {
+  constructor() {
+    this.bookshelf = [];
+  }
+  addBook(book) {
+    this.bookshelf.unshift(book);
+  }
+
+  removeBook(index) {
+    this.bookshelf.splice(index, 1);
+  }
+
+  deleteAllBooks() {
+    this.bookshelf.length = 0;
+  }
+}
+
+let libreria = new Library();
+
+libreria.addBook(new Book("The Hobbit", "J.R.R. Tolkien", 295, "Read"));
+
+libreria.addBook(
+  new Book("The Fellowship of the Ring", "J.R.R. Tolkien", 423, "Read")
+);
+
+libreria.addBook(
+  new Book("The NeverEnding Story", "Michael Ende", 368, "Read")
+);
+8
+libreria.addBook(
+  new Book(
+    "Little Bets: How Breakthrough Ideas Emerge from Small Discoveries",
+    "Peter Sims",
+    189,
+    "Not Read"
+  )
+);
+
+console.log(libreria.bookshelf);
 
 function addToLibrary(book) {
   myLibrary.unshift(book);
@@ -42,38 +82,38 @@ function removeAllBooks() {
 }
 
 function createBookElement(book) {
-  const bookDiv = document.createElement('div');
-  bookDiv.classList.add('book', 'card');
+  const bookDiv = document.createElement("div");
+  bookDiv.classList.add("book", "card");
   const bookIndex = myLibrary.indexOf(book);
-  bookDiv.setAttribute('data-index', bookIndex.toString());
+  bookDiv.setAttribute("data-index", bookIndex.toString());
 
-  const bookName = document.createElement('h2');
+  const bookName = document.createElement("h2");
   bookName.textContent = book.name;
 
-  const bookAuthor = document.createElement('h3');
+  const bookAuthor = document.createElement("h3");
   bookAuthor.textContent = book.author;
 
-  const bookPages = document.createElement('p');
-  bookPages.classList.add('book-pages');
+  const bookPages = document.createElement("p");
+  bookPages.classList.add("book-pages");
   bookPages.textContent = `Pages: ${book.pages}`;
 
-  const bookIsRead = document.createElement('p');
-  bookIsRead.classList.add('book-is-read');
-  bookIsRead.textContent = 'Status: ';
-  const bookStatus = document.createElement('span');
-  bookStatus.classList.add('book-status');
+  const bookIsRead = document.createElement("p");
+  bookIsRead.classList.add("book-is-read");
+  bookIsRead.textContent = "Status: ";
+  const bookStatus = document.createElement("span");
+  bookStatus.classList.add("book-status");
   bookStatus.textContent = book.isRead;
   bookIsRead.appendChild(bookStatus);
 
-  const buttonsContainer = document.createElement('div');
-  buttonsContainer.classList.add('book-el-buttons-container');
-  const removeButton = document.createElement('button');
-  removeButton.textContent = 'Remove';
-  removeButton.classList.add('remove-book');
+  const buttonsContainer = document.createElement("div");
+  buttonsContainer.classList.add("book-el-buttons-container");
+  const removeButton = document.createElement("button");
+  removeButton.textContent = "Remove";
+  removeButton.classList.add("remove-book");
 
-  const changeStatusButton = document.createElement('button');
-  changeStatusButton.textContent = 'Change Read Status';
-  changeStatusButton.classList.add('change-read-status');
+  const changeStatusButton = document.createElement("button");
+  changeStatusButton.textContent = "Change Read Status";
+  changeStatusButton.classList.add("change-read-status");
 
   buttonsContainer.appendChild(removeButton);
   buttonsContainer.appendChild(changeStatusButton);
@@ -88,7 +128,7 @@ function createBookElement(book) {
 }
 
 function displayBooks(arr) {
-  ui.libraryContainer.innerHTML = '';
+  ui.libraryContainer.innerHTML = "";
   arr.forEach((book) => {
     const bookDiv = createBookElement(book);
     ui.libraryContainer.appendChild(bookDiv);
@@ -99,15 +139,15 @@ function displayBooks(arr) {
 }
 
 function changeReadStatus() {
-  ui.libraryContainer.addEventListener('click', (event) => {
+  ui.libraryContainer.addEventListener("click", (event) => {
     const target = event.target;
-    if (target.classList.contains('change-read-status')) {
-      const bookDiv = target.closest('.card');
-      const bookIndex = parseInt(bookDiv.getAttribute('data-index'));
-      const bookStatus = bookDiv.querySelector('.book-status');
+    if (target.classList.contains("change-read-status")) {
+      const bookDiv = target.closest(".card");
+      const bookIndex = parseInt(bookDiv.getAttribute("data-index"));
+      const bookStatus = bookDiv.querySelector(".book-status");
 
       myLibrary[bookIndex].isRead =
-        myLibrary[bookIndex].isRead === 'Read' ? 'Not Read' : 'Read';
+        myLibrary[bookIndex].isRead === "Read" ? "Not Read" : "Read";
       bookStatus.textContent = myLibrary[bookIndex].isRead;
     }
   });
@@ -128,19 +168,22 @@ function changeReadStatus() {
 
 function removeBookFromLibrary() {
   const libraryContainer = ui.libraryContainer;
-  const bookDivs = libraryContainer.querySelectorAll('.card');
+  const bookDivs = libraryContainer.querySelectorAll(".card");
   for (let i = 0; i < bookDivs.length; i++) {
     const bookCard = bookDivs[i];
-    const removeButton = bookCard.querySelector('.remove-book');
-    removeButton.addEventListener('click', function () {
+    const removeButton = bookCard.querySelector(".remove-book");
+    removeButton.addEventListener("click", function () {
       // Get the book index from the data-index attribute of the book card
-      const bookIndex = parseInt(bookCard.getAttribute('data-index'));
+      const bookIndex = parseInt(bookCard.getAttribute("data-index"));
 
       // Remove the book card from the DOM
       bookCard.remove();
 
       // Remove the book object from the myLibrary array
-      myLibrary.splice(bookIndex, 1);
+      // myLibrary.splice(bookIndex, 1);
+
+      libreria.removeBook(bookIndex, 1);
+      console.log(libreria.bookshelf);
 
       console.log(`Book ${bookIndex} removed`);
     });
@@ -148,28 +191,31 @@ function removeBookFromLibrary() {
 }
 
 function main() {
-  ui.addBookButton.addEventListener('click', () => {
+  ui.addBookButton.addEventListener("click", () => {
     ui.addBookDialog.showModal();
   });
 
-  ui.formCancelButton.addEventListener('click', () => {
+  ui.formCancelButton.addEventListener("click", () => {
     ui.dialog.close();
   });
 
-  ui.formSubmitButton.addEventListener('click', (e) => {
+  ui.formSubmitButton.addEventListener("click", (e) => {
     e.preventDefault();
-    const bookName = document.querySelector('#book-name').value;
-    const bookAuthor = document.querySelector('#book-author').value;
-    const bookPages = document.querySelector('#book-pages').value;
-    const bookIsRead = ui.readStatusInput.checked ? 'Read' : 'Not Read';
+    const bookName = document.querySelector("#book-name").value;
+    const bookAuthor = document.querySelector("#book-author").value;
+    const bookPages = document.querySelector("#book-pages").value;
+    const bookIsRead = ui.readStatusInput.checked ? "Read" : "Not Read";
 
     const book = new Book(bookName, bookAuthor, bookPages, bookIsRead);
-    addToLibrary(book);
+    // addToLibrary(book);
+    libreria.addBook(book);
     ui.addBookDialog.close();
   });
 
-  ui.removeAllBooksButton.addEventListener('click', () => {
-    removeAllBooks();
+  ui.removeAllBooksButton.addEventListener("click", () => {
+    // removeAllBooks();
+
+    libreria.deleteAllBooks();
   });
 
   removeBookFromLibrary();
